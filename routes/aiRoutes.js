3419f8cd5bf19e8e17e18a00ac5955c9ac58c3f1
krish732006 <gandhikrish305@gmail.com>
2026-05-04 16:57:23 +0530

    fix gemini model and api error

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -24,7 +24,7 @@ router.post("/image", upload.single("image"), async (req, res) => {
     const base64Image = req.file.buffer.toString("base64");
 
     const result = await ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-1.5-flash",
       contents: [
         {
           role: "user",
